c6b0e6f02d89d3d83d334b97230b173731d5f3cb
sofia <sofiagarfo@hotmail.com>
2026-06-04 14:54:55 +0200

    add update() to towers and tower manager for attack

--- core/src/main/java/heig/vd/Tower/Tower.java
+++ core/src/main/java/heig/vd/Tower/Tower.java
@@ -2,19 +2,25 @@ package heig.vd.Tower;
 
 import heig.vd.mob.*;
 import heig.vd.utils.Position;
+import java.util.List;
 
 public class Tower {
     private Position position;
     private CombatTowerType type;
+    private float cooldownTimer = 0f;
 
     public Tower(Position position, CombatTowerType type) {
         this.position = position;
         this.type = type;
+
     }
 
     public boolean isInRange(Mob mob) {
-        return false;
-        //TODO
+        if (mob.getPosition() == null) {
+            return false;
+        }
+        double distance = position.distanceTo(mob.getPosition());
+        return distance <= type.getRange();
     }
 
     public void attack(Mob mob) {
@@ -39,4 +45,39 @@ public class Tower {
     public int getRange() { return type.getRange(); }
 
     public int getFireRate() { return type.getFireRate(); }
+
+    /**
+     * Look for mobs in rage and attack if we can
+     * @param delta
+     * @param mobs
+     */
+    public void update(float delta, List<Mob> mobs) {
+        // Decrement cooldown timer
+        cooldownTimer -= delta;
+
+        if (cooldownTimer <= 0) {
+            Mob target = findTarget(mobs);
+            if (target != null) {
+                attack(target);
+
+                // resets cooldown timer
+                // each fire rate is two time previous than the one before
+                // currently level 1 shoots one DMG per 3 seconds
+                cooldownTimer = 3.0f /(float)(Math.pow(2,getFireRate()-1)); // Reset cooldown based on fire rate;
+            }
+        }
+    }
+
+    private Mob findTarget(List<Mob> mobs) {
+        if (mobs == null || mobs.isEmpty()) {
+            return null;
+        }
+
+        for (Mob mob : mobs) {
+            if (isInRange(mob)) {
+                return mob;
+            }
+        }
+        return null;
+    }
 }
